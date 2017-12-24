01c2f967971bd1899950dc7545804da2a57dc77e
123012015163 <32355677+123012015163@users.noreply.github.com>
2017-12-24 20:17:25 +0800

    Android test3

---
+++ web2/README.md
@@ -0,0 +1,20 @@
+# Android第三次实验
+=======================================================
+自定义WebView验证隐式Intent的使用
+ 本实验通过自定义WebView加载URL来验证隐式Intent的使用。
+实验包含两个应用：
+1. 第一个应用：获取URL地址并启动隐式Intent的调用。
+2. 第二个应用：自定义WebView来加载URL
+
+
+
+
+------------------------------------------------------
+1. 截图
+    ![](https://github.com/123012015163/Android/blob/master/web2/img/1.png)
+    ![](https://github.com/123012015163/Android/blob/master/web2/img/2.png)
+    ![](https://github.com/123012015163/Android/blob/master/web2/img/3.png)
+    ![](https://github.com/123012015163/Android/blob/master/web2/img/4.png)
+    
+
+
---
+++ web2/app/src/main/java/com/fjnu/edu/web/BrowseActivity.java
@@ -0,0 +1,26 @@
+package com.fjnu.edu.web;
+
+/**
+ * Created by Dell on 2017/12/23.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class BrowseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse);
+        WebView wv = (WebView)findViewById(R.id.my_webview);//通过findViewById寻找元素
+        wv.loadUrl(url);//调用其loadUrl函数显示网页
+    }
+
+}
